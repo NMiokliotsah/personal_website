50f701a1096b06aaae898d8cd11ade2bf39dca08
Molosh Nikita <nmiokliotsah@gmail.com>
2020-07-25 12:36:16 +0300

    Implemented a contact sections and footer

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,22 @@
 import '../css/style.css';
 import * as $ from 'jquery';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'magnific-popup/dist/magnific-popup.css'
 import 'magnific-popup';
 
 $(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $(".navbar").addClass("sticky");
+            $(".goTop").fadeIn();
+        }
+        else {
+            $(".navbar").removeClass("sticky");
+            $(".goTop").fadeOut();
+        }
+    });
+
+    $(".goTop").click(function () { scroll(0, 0) });
     $(window).scroll(function () {
         this.scrollY > 20 ? $('.navbar').addClass('sticky') : $('.navbar').removeClass('sticky');
     });
@@ -16,6 +29,6 @@ $(document).ready(function () {
     $('.works').magnificPopup({
         delegate: 'a',
         type: 'image',
-        gallery: {enabled: true}
+        gallery: { enabled: true }
     });
 });
